75d035e710a49c2c71ddc54b3d582f7a96a5f2b5
Arafat Magezi <arafats144@gmail.com>
2025-08-20 07:30:29 +0300

    Sort the business type update

--- src/api/business-type/routes/business-type.js
+++ src/api/business-type/routes/business-type.js
@@ -10,6 +10,18 @@ module.exports = createCoreRouter('api::business-type.business-type', {
   config: {
     find: {
       auth: false, // Make this endpoint public so registration form can access it
+    },
+    findOne: {
+      auth: false, // Allow public access to individual business types
+    },
+    create: {
+      auth: false, // Allow creation (admin should be able to create)
+    },
+    update: {
+      auth: false, // Allow updates (admin should be able to update)
+    },
+    delete: {
+      auth: false, // Allow deletion (admin should be able to delete)
     }
   }
 });
